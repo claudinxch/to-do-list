6a62a3b66aaf8cb1fcc9a63a7f3c2304178f29c2
claudinxch <claudiohenrique0909@gmail.com>
2023-12-01 22:37:24 -0300

    first app version

---
+++ README.md
@@ -0,0 +1,32 @@
+# To-do list
+
+## An application to save a list of things you have to do.
+<p align="center">Simple application that allows the user to save an task, delete it, and check if it is done.</p>
+
+### Features
+- [x] Auto saving at the local storage.
+- [x] Possibility to check or delete a task.
+
+![My Image](./src/images/preview.png)
+
+### 🛠 Technologies
+
+The following tools were used to build this project:
+
+- JavaScript
+- HTML
+- CSS
+
+### Author
+---
+
+<a href="#">
+ <img style="border-radius: 50%;" src="https://avatars.githubusercontent.com/u/119335054?v=4" width="100px;" alt=""/>
+ <br />
+ <sub><b>Claudio Henrique</b></sub></a> <a href="#" title="Rocketseat">☕</a>
+
+
+Made by Claudio Henrique 👋🏽 get in touch!
+
+[![Linkedin Badge](https://img.shields.io/badge/-Claudio-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/claudio-m-246674288)](https://www.linkedin.com/in/claudio-m-246674288) 
+[![Gmail Badge](https://img.shields.io/badge/-claudiohenrique0909@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:claudiohenrique0909@gmail.com)](mailto:claudiohenrique0909@gmail.com)
---
+++ src/script.js
@@ -0,0 +1,66 @@
+const inputBox = document.querySelector('#input-box');
+const container = document.querySelector('.todo-app');
+let list = document.createElement('div');
+list.className = 'list';
+container.appendChild(list);
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert('You must write a task!');
+        inputBox.value = '';
+        inputBox.focus();
+    } else {
+        let tasks = document.createElement('div');
+        tasks.className = 'tasks';
+        tasks.innerHTML = `<div class="task">
+        <input type="checkbox" name="progress" class="progress">
+        <p class="task-description">${inputBox.value}</p>
+        </div>
+        <div class="task-actions">
+        <a class="action-button delete-button">
+        <i class="fa-regular fa-trash-can"></i>
+        </a>
+        </div>`;
+        list.style.marginTop = '0px';
+        list.appendChild(tasks);
+        inputBox.value = '';
+        inputBox.focus();
+
+        const progress = tasks.querySelector('.progress');
+        progress.addEventListener("change", () => {
+            const taskDescription = tasks.querySelector('.task-description');
+            taskDescription.classList.toggle('checked', progress.checked);
+        });
+
+        saveData();
+
+    }
+}
+
+inputBox.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        addTask();
+    }
+});
+
+list.addEventListener("click", function(e) {
+    if (e.target.tagName === 'I') {
+        let closestTask = e.target.closest('.tasks');
+
+        if (closestTask) {
+            console.log('Deleting task:', closestTask.querySelector('.task-description').textContent);
+            closestTask.remove();
+            saveData();
+        }
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", list.innerHTML);
+    console.log('salvou')
+}
+
+function showTasksSaved() {
+    list.innerHTML = localStorage.getItem("data");
+}
+showTasksSaved();
